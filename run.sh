aecca216b11b9365a8e57099331106dc596ebd12
Pawel Ruchaj <pawel.ruchaj@oath.com>
2018-12-10 10:55:28 +0000

    pecl mcrypt

--- run.sh
+++ run.sh
@@ -30,23 +30,23 @@ chown -R user:www-data /var/log/php /var/log/nginx
 
 cp -ar ${PERSISTENT_CONFIG_FOLDER}/* ${VOLATILE_CONFIG_FOLDER}
 
-iniSet /etc/php/7.0/mods-available/xdebug.ini "xdebug\.remote_host" $CTNR_HOST_IP
-iniSet /etc/php/7.0/mods-available/xdebug.ini "xdebug\.remote_port" $CTNR_HOST_XDEBUG_PORT
+iniSet /etc/php/7.2/mods-available/xdebug.ini "xdebug\.remote_host" $CTNR_HOST_IP
+iniSet /etc/php/7.2/mods-available/xdebug.ini "xdebug\.remote_port" $CTNR_HOST_XDEBUG_PORT
 
 if [ "$CTNR_APP_ENV" = "dev" ]; then
     echo "== CONTAINER IS STARTING IN DEV MODE =="
     phpenmod xdebug
-    iniSet /etc/php/7.0/fpm/php.ini display_errors On
-    iniSet /etc/php/7.0/fpm/php.ini display_startup_errors On
-    iniSet /etc/php/7.0/cli/php.ini display_errors On
-    iniSet /etc/php/7.0/cli/php.ini display_startup_errors On
+    iniSet /etc/php/7.2/fpm/php.ini display_errors On
+    iniSet /etc/php/7.2/fpm/php.ini display_startup_errors On
+    iniSet /etc/php/7.2/cli/php.ini display_errors On
+    iniSet /etc/php/7.2/cli/php.ini display_startup_errors On
 else
     echo "== CONTAINER IS STARTING IN PROD MODE =="
     phpdismod xdebug
-    iniSet /etc/php/7.0/fpm/php.ini display_errors Off
-    iniSet /etc/php/7.0/fpm/php.ini display_startup_errors Off
-    iniSet /etc/php/7.0/cli/php.ini display_errors Off
-    iniSet /etc/php/7.0/cli/php.ini display_startup_errors Off
+    iniSet /etc/php/7.2/fpm/php.ini display_errors Off
+    iniSet /etc/php/7.2/fpm/php.ini display_startup_errors Off
+    iniSet /etc/php/7.2/cli/php.ini display_errors Off
+    iniSet /etc/php/7.2/cli/php.ini display_startup_errors Off
 fi
 
 # start container
